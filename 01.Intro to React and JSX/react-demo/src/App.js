4abaff9874a91272a6b7990051c87bfd281be315
lentqq <47202527+lentqq@users.noreply.github.com>
2019-12-09 11:24:43 +0200

    Add files via upload

---
+++ 01.Intro to React and JSX/react-demo/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Spinner } from 'react-bootstarp';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* <Spinner animation="border" role="status">
+          <span className="sr-only">Loading...</span>
+        </Spinner> */}
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
